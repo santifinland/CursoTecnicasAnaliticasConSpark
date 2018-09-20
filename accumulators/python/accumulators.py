866bd973af8ba21636481102d8d6ea2d232408a3
Santiago Dobón Méndez Trelles <sdmt@tid.es>
2018-09-20 13:28:14 +0200

    [FEATURE] Adapt to pyton 3

--- accumulators/python/accumulators.py
+++ accumulators/python/accumulators.py
@@ -1,18 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import logging
 import os
 
 from pyspark.sql import SparkSession
 
-from common.logger_configuration import LoggerManager
-
-
-# Get application logger
-LoggerManager()
-logger = logging.getLogger()
-logger_spark = logging.getLogger('py4j')
-logger_spark.setLevel(logging.INFO)
+from common.logger_configuration import logger
 
 
 def main():
@@ -46,5 +38,5 @@ def acc(x, condition):
 if __name__ == "__main__":
     try:
         main()
-    except Exception, e:
+    except Exception as e:
         logger.error('Failed to execute process: {}'.format(e.message), exc_info=True)
