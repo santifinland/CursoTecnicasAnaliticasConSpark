2e27cf545848a97ab6de1ba6d6169c3d5bc324b2
Santiago Dobón <santifinland@gmail.com>
2021-06-24 08:56:33 +0200

    [REFACTOR] Filter

--- transformations/python/filter.py
+++ transformations/python/filter.py
@@ -23,17 +23,21 @@ def main():
 
     # Read csv injecting schema
     cdr: DataFrame = spark.read.csv('data/call_cdr/year=1924/month=04/day=19', header=True, schema=schema)
+    print("Initial number of records: {}".format(cdr.count()))
 
     # Filter international calls
     international_cdr: DataFrame = cdr.filter(col('INTERNATIONAL'))
+    print("Found {} records".format(international_cdr.count()))
     international_cdr.show()
 
     # Filter long calls
     long_cdr: DataFrame = cdr.filter(cdr.DURATION > 60)
+    print("Found {} records".format(long_cdr.count()))
     long_cdr.show()
 
     # Filter free calls
     free_cdr: DataFrame = cdr.filter('PRICE = 0.0')
+    print("Found {} records".format(free_cdr.count()))
     free_cdr.show()
 
 
