123047d21a90828c6dfed4d02e1d68b37089544d
Santiago Dobón <santifinland@gmail.com>
2021-10-27 09:33:35 +0200

    [REFACTOR] Modify accu

--- broadcast/python/broadcast.py
+++ broadcast/python/broadcast.py
@@ -4,14 +4,13 @@ import random
 
 from pyspark.sql import SparkSession
 
-from common.logger_configuration import logger
-
 
 def main():
-    logger.info(u"Broadcast variable basic usage")
+
+    print('Broadcast variable basic usage')
 
     # Create Spark Session
-    spark = SparkSession.builder.appName("Spark Course. Broadcast variable basic usage").getOrCreate()
+    spark = SparkSession.builder.appName('Spark Course. Broadcast variable basic usage').getOrCreate()
 
     # Get security token. i.e. oAuth token
     token = authorize()
@@ -20,20 +19,20 @@ def main():
     broadcasted_token = spark.sparkContext.broadcast(token)
 
     # Use broadcast token to access resource from the nodes
-    res = accessResource(broadcasted_token.value)
-    logger.info(res)
+    res = access_resource(broadcasted_token.value)
+    print(res)
 
 
 def authorize():
     return random.randint(1, 10)
 
 
-def accessResource(token):
-    return "Resource accessed using token {}".format(token)
+def access_resource(token):
+    return 'Resource accessed using token {}'.format(token)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:
         main()
     except Exception as e:
-        logger.error('Failed to execute process: {}'.format(e), exc_info=True)
+        print('Failed to execute process: {}'.format(e))
