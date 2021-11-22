a85cbfb11143d6145d085d9ef9fc9812cc928a0d
Santiago Dobón <santifinland@gmail.com>
2021-11-22 16:44:53 +0100

    [FEATURE] Add windows

--- windows/python/window.py
+++ windows/python/window.py
@@ -1,41 +1,35 @@
 # -*- coding: utf-8 -*-
 
-import os
-
-from pyspark.sql import SparkSession, Window
-from pyspark.sql.functions import avg, col, rank
-from pyspark.sql.types import FloatType
-
-from common.logger_configuration import logger
+from pyspark.sql import DataFrame, SparkSession, Window
+from pyspark.sql.functions import avg, rank
 
 
 def main():
-    logger.info(u"Window Functions")
+
+    print(u"Window Functions")
 
     # Create Spark Session
     spark = SparkSession.builder.appName("Spark Course. Window Functions").getOrCreate()
 
-    # Load CDR data
-    path = os.path.join(os.environ["HOME"], "data", "cdr")
-    cdr = spark.read.parquet(path) \
-        .select("TRAFFIC_EVENT_TYPE_ID", col("SESSION_DURATION_QT").cast(FloatType()))
+    # Read data
+    cdr: DataFrame = spark.read.option('header', 'true').csv('data/call_cdr/year=1924/month=04/day=19')
     cdr.show()
 
     # Create Window Function
-    w1 = Window.partitionBy("TRAFFIC_EVENT_TYPE_ID")
-    duration_avg = avg("SESSION_DURATION_QT").over(w1)
-    cdr.withColumn("duration_avg", duration_avg).show()
+    w1 = Window.partitionBy("CALLER")
+    duration_avg = avg("DURATION").over(w1)
+    cdr.withColumn("DURATION_AVG", duration_avg).show()
 
     # Create another Window Function
     w2 = Window \
-        .partitionBy("TRAFFIC_EVENT_TYPE_ID") \
-        .orderBy("SESSION_DURATION_QT")
+        .partitionBy("CALLER") \
+        .orderBy("DURATION")
     rank_duration = rank().over(w2)
-    cdr.withColumn("rank_duration", rank_duration).show()
+    cdr.withColumn("RANK_DURATION", rank_duration).show()
 
 
 if __name__ == "__main__":
     try:
         main()
     except Exception as e:
-        logger.error('Failed to execute process: {}'.format(e), exc_info=True)
+        print('Failed to execute process: {}'.format(e))
